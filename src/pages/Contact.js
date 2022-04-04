5814c67e8a485b8bdddeab885fb091c81d440cc7
Lara Potjewyd <larapotjewyd@gmail.com>
2022-04-04 12:52:01 -0600

    fetching data in contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../components/Navigation';
-import Form from '../components/Form';
 import TextBlock from '../components/TextBlock';
+import Form from '../components/Form';
+import { get } from '../apiController';
 
 
 export default function Contact() {
+  const [ pageContent, setContent ] = useState({ title: '', content: '' });
+  const [ currentPage ] = useState( 'contact' )
+
+  useEffect(() => {
+    const getData = async () => {
+      const apiData = await get(currentPage);
+      setContent({
+        title: apiData.data[0].title,
+        content: apiData.data[0].content,
+      })
+    }
+    getData();
+  }, [currentPage])
+  
+  const { title, content } = pageContent;
   
     return(
       <div className='contact_wrapper'>
 
-        <Navigation linkTo='home' currentPage='contact' />
+        <Navigation linkTo='home' />
 
         <div className='contact_container'>
           <div className='textblock_wrapper contact-textblock'>
-            <TextBlock />
+            <TextBlock title={title} content={content} />
           </div>
 
           <div className='form_wrapper'>

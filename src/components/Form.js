25aa412a359e0aaa493641777975babfde1db390
Lara Potjewyd <larapotjewyd@gmail.com>
2022-04-05 17:19:15 -0600

    finished form validation and success message

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,15 +3,27 @@ import { post } from '../apiController';
 
 export default function Form() {
 
-  const [ formValues, setFormValues ] = useState({
+  const defaultErrorState = {
+    first_name: false,
+    last_name: false,
+    title: false,
+    email: false,
+    message: false
+  }
+
+  const defaultFormValues = {
     first_name: '',
     last_name: '',
     title: '',
     email: '',
     message: ''
-  })
-  const [ submitted, setSubmitted ] = useState(false);
-  const [valid, setValid] = useState(false);
+  }
+
+  const [ formValues, setFormValues ] = useState(defaultFormValues)
+  const [ hasInputError, setInputError ] = useState(defaultErrorState)
+  const [ showSuccess, setSuccess ] = useState(false);
+
+  const { first_name, last_name, title, email, message } = formValues
 
   const handleChange = (event) => {
     const value = event.target.value;
@@ -22,83 +34,100 @@ export default function Form() {
     })
   }
 
+  const validateForm = () => {
+    const formErrors = {};
+    if(!first_name) {
+      formErrors['first_name'] = true
+    }
+    if(!last_name) {
+      formErrors['last_name'] = true
+    }
+    if(!title) {
+      formErrors['title'] = true
+    }
+    if(!email) {
+      formErrors['email'] = true
+    }
+    if(!message) {
+      formErrors['message'] = true
+    }
+    return formErrors;
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(first_name) {
-      setValid(true);
-      post(formValues);
+    const formErrors = validateForm();
+
+    if(Object.keys(formErrors).length) {
+      setInputError(formErrors);
+      return;
     }
-    setSubmitted(true);
+    post(formValues);
+    setSuccess(true);
     handleReset();
   }
 
   const handleReset = () => {
-    setFormValues({
-      first_name: '',
-      last_name: '',
-      title: '',
-      email: '',
-      message: ''
-    })
+    setFormValues(defaultFormValues);
+    setInputError(defaultErrorState);
+    setTimeout(() => setSuccess(false), 6000);
   }
-
-  const { first_name, last_name, title, email, message } = formValues
-
+  
   return (
     <>
-    <h2 className='form_title title--h2'>Heading Two</h2>
+    <h2 className='form_title title--h2'>Contact</h2>
     <form className='form_container'>
       <div className='form_input_container input--resize'>
         <input 
-          className='form_input' 
+          className={`form_input ${hasInputError.first_name && !first_name && 'input--error'}`} 
           type='text' 
           placeholder='First Name'
           name='first_name'
           value={first_name} 
           onChange={handleChange} 
         />
-        {submitted && !first_name && <span className='input--error--text'>required</span>}       
+        {hasInputError.first_name && !first_name && <span className='input--error--text'>required</span>}       
       </div>
 
       <div className='form_input_container input--resize'>
         <input 
-          className='form_input' 
+          className={`form_input ${hasInputError.last_name && !last_name && 'input--error'}`} 
           type='text' 
           placeholder='Last Name' 
           name='last_name'
           value={last_name} 
           onChange={handleChange}
         />
-        {submitted && !last_name && <span className='input--error--text'>required</span>}       
+        {hasInputError.last_name && !last_name && <span className='input--error--text'>required</span>}       
       </div>
 
       <div className='form_input_container input--resize'>
         <input 
-          className='form_input' 
+          className={`form_input ${hasInputError.title && !title && 'input--error'}`} 
           type='text' 
           placeholder='Title' 
           name='title'
           value={title} 
           onChange={handleChange} 
         />
-        {submitted && !title && <span className='input--error--text'>required</span>}       
+        {hasInputError.title && !title && <span className='input--error--text'>required</span>}       
       </div>
 
       <div className='form_input_container input--resize'>
         <input 
-          className='form_input' 
+          className={`form_input ${hasInputError.email && !email && 'input--error'}`} 
           type='email' 
           placeholder='Email'
           name='email'
           value={email} 
           onChange={handleChange} 
         />
-        {submitted && !email && <span className='input--error--text'>required</span>}       
+        {hasInputError.email && !email && <span className='input--error--text'>required</span>}       
       </div>
 
       <div className='form_input_container input--full'>
         <textarea 
-          className='form_input' 
+          className={`form_input ${hasInputError.message && !message && 'input--error'}`} 
           type='text' 
           rows='8'
           placeholder='Message' 
@@ -106,11 +135,11 @@ export default function Form() {
           value={message} 
           onChange={handleChange}
         />
-        {submitted && !email && <span className='input--error--text'>required</span>}       
+        {hasInputError.message && !message && <span className='input--error--text'>required</span>}       
       </div>
-      {valid && <p>submitted!</p>}
-      <button className='form_button button button--primary' onClick={handleSubmit} >Submit</button>
 
+      <button className='form_button button button--primary' onClick={handleSubmit} >Submit</button>
+      <p className={`form--success--text text--paragraph ${showSuccess && 'show--success'}`}>Message sent</p>
     </form>
   </>
   )
